c2b573ffb463c521c416f241d80e9206be25f3b4
Alonso <alonsonogueira036@gmail.com>
2025-08-08 23:22:28 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,71 @@
+// import {createServer} from 'node:http'
+
+// const server = createServer((request, response)=>{
+//     response.write('Hello world')
+//     return response.end()
+// })
+// server.listen(3333)
+
+//GET: operaçao de busca de informacao
+//POST: criar registros
+//PUT: Alterar
+//DELETE: deletar
+
+
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply)=> {
+
+    const {title, description, duration} = request.body
+
+    database.creat({
+        title,
+        description,
+        duration,
+    })
+
+    console.log(database.list())
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', (request)=>{
+    const search = request.query.search
+
+    console.log(search)
+
+    const videos = database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', (request,reply)=>{
+    const videoID = request.params.id
+    const {title, description, duration} = request.body
+
+    const video = database.update(videoID, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    
+    database.delete(videoId)
+
+    return reply.status(204).send()
+
+})
+
+server.listen({
+    port: 3333
+})
